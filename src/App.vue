e437ba75a3786e06cdefd1f2262099f439f61e91
王浩杰 <wanghaojie1_zb@cmos.chinamobile.com>
2023-12-08 02:08:29 +0800

    Revert "11"
    This reverts commit 524661ac6bd03e8c79f4b9bd93269c871300d275.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
   </div>
 </template>
 <script>
-console.log(1)
 </script>
 
 <style lang="less">
@@ -20,14 +19,14 @@ console.log(1)
     text-align: center;
     color: #2c3e50;
   }
-
+  
   #nav {
     padding: 30px;
-
+    
     a {
       font-weight: bold;
       color: #2c3e50;
-
+      
       &.router-link-exact-active {
         color: #42b983;
       }
